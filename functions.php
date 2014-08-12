9ce0e355d7cd99d3c5ffdf790c787cdc4bf28ce0
Ivan Ivanić <spoygg@spoygg.com>
2014-08-12 02:46:04 +0200

    Fixing element styles rules. Updating settings file to new format.

--- functions.php
+++ functions.php
@@ -7,13 +7,9 @@ include_once get_template_directory() . '/library/class_upfront_theme.php';
 
 class Uf_fixer extends Upfront_ChildTheme {
     public function initialize() {
-         if (!upfront_is_builder_running()) {
-            add_filter('upfront_get_layout_properties', array($this, 'get_layout_properties'), 10, 2);
-        }
         add_filter('upfront_augment_theme_layout', array($this, 'augment_layout'));
         $this->add_actions_filters();
         $this->populate_pages();
-        $this->install_element_alternative_styles();
         add_filter("comment_form_field_author", array( $this, "comment_form_author" ));
         add_filter("comment_form_field_email", array( $this, "comment_form_email" ));
         add_filter("comment_form_field_url", array( $this, "comment_form_website" ));
@@ -33,10 +29,6 @@ class Uf_fixer extends Upfront_ChildTheme {
 
     }
 
-    protected function install_element_alternative_styles(){
-        $this->import_element_styles();
-    }
-
     protected function add_actions_filters() {
         // Include current theme style
         add_action('wp_head', array($this, 'enqueue_styles'), 200);
@@ -44,7 +36,6 @@ class Uf_fixer extends Upfront_ChildTheme {
 
     public function enqueue_styles() {
         wp_enqueue_style('current_theme', get_stylesheet_uri());
-
     }
 
     /**
@@ -100,41 +91,6 @@ FIELD;
         </p>
 FIELD;
         return $field;
-    }
-
-    public function get_layout_properties($properties, $args) {
-        if ($this->incorrect_stylesheet($args['stylesheet'])) return $properties;
-
-        $this->theme = $args['stylesheet'];
-
-        $settings_file = sprintf(
-            '%ssettings.php',
-            trailingslashit(dirname(__FILE__))
-        );
-        if (file_exists($settings_file)) {
-            include $settings_file;
-        }
-        if (!empty($layout_properties)) {
-            $properties = json_decode(stripslashes($layout_properties), true);
-        }
-        if (!empty($typography)) {
-            $properties[] = array(
-                'name' => 'typography',
-                'value' => json_decode(stripslashes($typography))
-            );
-        }
-        if (!empty($layout_style)) {
-            $properties[] = array(
-                'name' => 'layout_style',
-                'value' => addslashes($layout_style)
-            );
-        }
-
-        return $properties;
-    }
-    protected function incorrect_stylesheet($stylesheet) {
-        if(empty($stylesheet) || $stylesheet === 'theme' || $stylesheet === 'upfront') return true;
-        return false;
     }
 
     public function augment_layout ($layout) {
--- settings.php
+++ settings.php
@@ -1,50 +1,49 @@
-<?php $typography = '{\"h1\":{\"weight\":\"900\",\"style\":\"normal\",\"size\":\"109\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\",\"color\":\"rgba(51,51,51,1)\"},\"h2\":{\"weight\":\"900\",\"style\":\"normal\",\"size\":\"28\",\"line_height\":\"0\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\"},\"h3\":{\"weight\":\"200\",\"style\":\"normal\",\"size\":\"28\",\"line_height\":\"0\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\",\"color\":\"rgba(51,51,51,1)\"},\"h4\":{\"weight\":\"500\",\"style\":\"normal\",\"size\":\"28\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\"}}';
-$layout_style = '#usearch #usearch-archive-search .upfront-object-content{    width:630px;}input.search-field{width:530px;height:50px;float:left;padding:0;border:none; line-height:50px;padding-left:22px;}button.search-button{float:left;border:none;height:50px;width:50px;width:;text-indent:9999px;overflow:hidden;}#contact
- #ucontact-home-footer-contact .submit-field{
-    float:right;
-    background:#645191;
-    border:none;
-    padding:10px 20px;
-    color:#fff;
-    margin:15px 25px;
-}
+<?php
+return array(
+	'typography' => '{\"h1\":{\"weight\":\"900\",\"style\":\"normal\",\"size\":\"109\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\",\"color\":\"rgba(51,51,51,1)\"},\"h2\":{\"weight\":\"900\",\"style\":\"normal\",\"size\":\"28\",\"line_height\":\"0\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\"},\"h3\":{\"weight\":\"200\",\"style\":\"normal\",\"size\":\"28\",\"line_height\":\"0\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\",\"color\":\"rgba(51,51,51,1)\"},\"h4\":{\"weight\":\"500\",\"style\":\"normal\",\"size\":\"28\",\"font_face\":\"Raleway\",\"font_family\":\"sans-serif\"}}',
+	'layout_style' => '#usearch #usearch-archive-search .upfront-object-content{    width:630px;}input.search-field{width:530px;height:50px;float:left;padding:0;border:none; line-height:50px;padding-left:22px;}button.search-button{float:left;border:none;height:50px;width:50px;width:;text-indent:9999px;overflow:hidden;}#contact
+	 #ucontact-home-footer-contact .submit-field{
+			float:right;
+			background:#645191;
+			border:none;
+			padding:10px 20px;
+			color:#fff;
+			margin:15px 25px;
+	}
 
-  #ucontact-home-footer-contact .ucontact-input{
-    height:50px;
-    margin:0;
-    padding:15px;
-    line-height:50px;
-}
+		#ucontact-home-footer-contact .ucontact-input{
+			height:50px;
+			margin:0;
+			padding:15px;
+			line-height:50px;
+	}
 
- ::-webkit-input-placeholder {
-   color: #343434;
-}
+	 ::-webkit-input-placeholder {
+		 color: #343434;
+	}
 
- :-moz-placeholder
-{
-   color: #343434;
-}
+	 :-moz-placeholder
+	{
+		 color: #343434;
+	}
 
- ::-moz-placeholder 
-{
-   color: #343434;
-}
+	 ::-moz-placeholder
+	{
+		 color: #343434;
+	}
 
- :-ms-input-placeholder {
-   color: #343434;
-}
+	 :-ms-input-placeholder {
+		 color: #343434;
+	}
 
-  #ucontact-home-footer-contact .upfront-field-container{
-    margin:0;
-}
+		#ucontact-home-footer-contact .upfront-field-container{
+			margin:0;
+	}
 
-  #ucontact-home-footer-contact .textarea-field{
-    height:150px;
-}
-#footer-text-boxes .upfront-plain_txt h4{
-        margin:0;
-    }
-';
-$theme_fonts = '';
-$theme_colors = '';
-$layout_properties = '[]';
+		#ucontact-home-footer-contact .textarea-field{
+			height:150px;
+	}',
+	'theme_fonts' => '',
+	'theme_colors' => '',
+	'layout_properties' => '[]'
+);
