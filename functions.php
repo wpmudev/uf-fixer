b17fd98d4e51f422dd8cfb73e67a53fe15926be3
Sergey Kolesov <sergey@incsub.com>
2014-09-12 12:39:53 +0700

    remove the entire _import_image_slider()

--- functions.php
+++ functions.php
@@ -119,39 +119,6 @@ FIELD;
         $this->_slider_imported = true;
         return $regions;
     }
-
-    private function _import_slider_image ($filepath) {
-        $filepath = trailingslashit(get_stylesheet_directory()) . $filepath;
-        $key = $this->get_prefix() . '-slider-images';
-        $images = get_option($key, array());
-        if (!empty($images[$filepath])) return $images[$filepath];
-        if (!file_exists($filepath)) return $filepath;
-        // else import image
-        $wp_upload_dir = wp_upload_dir();
-        $pfx = !empty($wp_upload_dir['path']) ? trailingslashit($wp_upload_dir['path']) : '';
-        if (!function_exists('wp_generate_attachment_metadata')) require_once(ABSPATH . 'wp-admin/includes/image.php');
-        $filename = basename($filepath);
-        while (file_exists("{$pfx}{$filename}")) {
-            $filename = rand() . $filename;
-        }
-        @copy($filepath, "{$pfx}{$filename}");
-        $wp_filetype = wp_check_filetype(basename($filename), null);
-        $attachment = array(
-            'guid' => $wp_upload_dir['url'] . '/' . basename($filename),
-            'post_mime_type' => $wp_filetype['type'],
-            'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
-            'post_content' => '',
-            'post_status' => 'inherit'
-        );
-        $attach_id = wp_insert_attachment($attachment, "{$pfx}{$filename}");
-        $attach_data = wp_generate_attachment_metadata( $attach_id, "{$pfx}{$filename}" );
-        wp_update_attachment_metadata( $attach_id, $attach_data );
-
-        $images[$filepath] = $attach_id;
-        update_option($key, $images);
-
-        return $attach_id;
-    }
 }
 
 Uf_fixer::serve();
