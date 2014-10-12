d0a363d5feced5ff8cb78d9340421b6f6c9c2eb2
Sergey Kolesov <sergey@incsub.com>
2014-10-12 20:37:28 +0700

    testing page with broken slider

---
+++ layouts/single-page-testing.php
@@ -0,0 +1,30 @@
+<?php
+if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'header.php');
+
+$main = upfront_create_region(
+			array(
+"name" => "main", 
+"title" => "Main Area", 
+"type" => "wide", 
+"scope" => "local", 
+"container" => "main", 
+"position" => 10, 
+"allow_sidebar" => true
+),
+			array(
+"row" => 140, 
+"background_type" => "slider", 
+"background_color" => "#c5d0db", 
+"nav_region" => "", 
+"background_slider_rotate" => true, 
+"background_slider_rotate_time" => 5, 
+"background_slider_control" => "always", 
+"background_slider_transition" => "crossfade", 
+"background_slider_images" => array("/images/logo.png", "/images/26072008.jpg")
+)
+			);
+
+$regions->add($main);
+
+if (file_exists(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'footer.php')) include(get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'global-regions' . DIRECTORY_SEPARATOR . 'footer.php');
+
