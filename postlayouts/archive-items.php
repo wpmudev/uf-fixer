53c8d4d61707af57ff20fd0458754583c2d2f46f
Sergey Kolesov <sergey@incsub.com>
2014-10-17 23:44:12 +0700

    archive posts layout

--- postlayouts/archive-items.php
+++ postlayouts/archive-items.php
@@ -1,20 +1,44 @@
 <?php return array(
 "postLayout" => array(array(
 		"objects" => array(array(
-				"classes" => "post-part 24", 
+				"classes" => "c17 post-part ml0 mt0", 
 				"slug" => "title"
 				)), 
-		"classes" => "c0 clr"
+		"classes" => "c17 clr"
 		), array(
 		"objects" => array(array(
-				"classes" => " post-part c24", 
-				"slug" => "contents"
+				"classes" => "c17 post-part ml0 mt2", 
+				"slug" => "excerpt"
 				)), 
-		"classes" => "c24 clr"
+		"classes" => "c17 clr"
+		), array(
+		"objects" => array(array(
+				"classes" => "c5 post-part ml0 mt2", 
+				"slug" => "date"
+				)), 
+		"classes" => "c5 clr"
+		), array(
+		"objects" => array(array(
+				"classes" => "c6 post-part ml0 mt2", 
+				"slug" => "author"
+				)), 
+		"classes" => "c6  "
 		)), 
 "partOptions" => array(
 	"featured_image" => array(
-		"height" => "100"
+		"height" => "289", 
+		"attributes" => array(
+			"style" => "max-height: 289px"
+			)
+		), 
+	"date" => array(
+		"format" => "F m, Y"
+		), 
+	"excerpt" => array(
+		"anchor" => ""
+		), 
+	"author" => array(
+		"anchor" => ""
 		)
 	)
 );
