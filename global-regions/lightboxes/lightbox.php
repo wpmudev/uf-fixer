2b0b943cbf207d59f3aad8edc202080c79139b71
Gagan Goraya <gagan@incsub.com>
2015-07-23 13:17:57 +0530

    burger menu push fix
    https://app.asana.com/0/9056903824374/41042575032933

--- global-regions/lightboxes/lightbox.php
+++ global-regions/lightboxes/lightbox.php
@@ -1,33 +1,29 @@
 <?php
 $lightbox = upfront_create_region(
-			array (
-  'name' => 'lightbox',
-  'title' => 'lightbox Region',
-  'type' => '',
-  'scope' => '',
-  'container' => 'lightbox',
-  'position' => 1,
-  'allow_sidebar' => true,
+			array(
+"name" => "lightbox", 
+"title" => "lightbox Region", 
+"type" => "", 
+"scope" => "", 
+"container" => "lightbox", 
+"position" => 1, 
+"allow_sidebar" => true
 ),
-			array (
-  'breakpoint' => 
-  (array)(array(
-     'tablet' => 
-    (array)(array(
-       'edited' => false,
-       'col' => 24,
-    )),
-     'mobile' => 
-    (array)(array(
-       'edited' => false,
-       'col' => 24,
-    )),
-     'custom-1410783666947' => 
-    (array)(array(
-       'edited' => false,
-       'col' => 24,
-    )),
-  )),
+			array(
+"breakpoint" => array(
+	"tablet" => array(
+		"edited" => false, 
+		"col" => 24
+		), 
+	"mobile" => array(
+		"edited" => false, 
+		"col" => 24
+		), 
+	"custom-1410783666947" => array(
+		"edited" => false, 
+		"col" => 24
+		)
+	)
 )
 			);
 
